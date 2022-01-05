ff253b2f27e1a03762acec9adda8b1cf18f41eab
huanghao <1308936944@qq.com>
2022-01-05 14:06:04 +0800

    blog页面侧边栏菜单生成算法

--- src/components/blogSider/index.tsx
+++ src/components/blogSider/index.tsx
@@ -1,89 +1,40 @@
-import { Layout, Menu } from '@arco-design/web-react';
-import { IconApps, IconBulb, IconBug } from '@arco-design/web-react/icon';
+import React from 'react';
 
-const MenuItem = Menu.Item;
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup
+import { Menu } from '@arco-design/web-react';
+
+import blogMenu from '../../router/path'
+
+function createBlogMenu(c: any): any {
+
+  if (!Array.isArray(c.children)) {
+    return c.title && c.title || ''
+  }
+  return c.children.map((cEle: any) => {
+    return React.createElement(
+      cEle.compName, 
+      {
+        key: cEle.idx, 
+        title: React.createElement(React.Fragment, null, cEle.icoName ? React.createElement(cEle.icoName, null) : '', cEle.title),
+        to: cEle.title ? null : cEle.idx
+      },
+      ...createBlogMenu(cEle))
+  })
+}
+
 
 function BlogSider(props: any) { 
 
-
   return <div
-  className='menu-demo'
   style={{
-    height: 600,
+    height: 600
   }}
 >
   <Menu
     style={{ width: 200, height: '100%' }}
-    defaultOpenKeys={['0']}
-    defaultSelectedKeys={['0_1']}
   >
-    <SubMenu
-      key='0'
-      title={
-        <>
-          <IconApps /> 数据结构与算法
-        </>
-      }
-    >
-      <MenuItem key='0_0'>基础</MenuItem>
-      <MenuItem key='0_1'>应用</MenuItem>
-      <MenuItem key='0_2' disabled>
-        Menu 3
-      </MenuItem>
-    </SubMenu>
-    <SubMenu
-      key='1'
-      title={
-        <>
-          <IconBug /> Navigation 2
-        </>
-      }
-    >
-      <MenuItem key='1_0'>Menu 1</MenuItem>
-      <MenuItem key='1_1'>Menu 2</MenuItem>
-      <MenuItem key='1_2'>Menu 3</MenuItem>
-    </SubMenu>
-    <SubMenu
-      key='2'
-      title={
-        <>
-          <IconBulb /> Navigation 3
-        </>
-      }
-    >
-      <MenuItemGroup key='2_0' title='Menu Group 1'>
-        <MenuItemGroup key='2_0_0' title="mg11">
-          <MenuItem key='2_0_0_0'>Menu 1</MenuItem>
-          <MenuItem key='2_0_0_1'>Menu 2</MenuItem>
-        </MenuItemGroup>
-        <MenuItemGroup  key='2_0_1' title="mg12">
-          <MenuItem key='2_0_1_0'>Menu 1</MenuItem>
-          <MenuItem key='2_0_1_1'>Menu 2</MenuItem>
-        </MenuItemGroup>
-      </MenuItemGroup>
-      <MenuItemGroup key='2_1' title='Menu Group 1'>
-        <MenuItem key='2_1_0'>Menu 3</MenuItem>
-        <MenuItem key='2_1_1'>Menu 4</MenuItem>
-      </MenuItemGroup>
-    </SubMenu>
-    <SubMenu key='3' title={
-      <>
-        <IconBulb /> 标题4
-      </>
-    }>
-      <SubMenu key='3_0' title={
-        <>
-          <IconBulb /> 标题4_3
-        </>
-      }>
-        <MenuItemGroup key='3_0_0' title="标题xx">
-          <MenuItem key='3_0_0_0'>标题xx1</MenuItem>
-          <MenuItem key='3_0_0_1'>标题xx2</MenuItem>
-        </MenuItemGroup>
-      </SubMenu>
-    </SubMenu>
+    {
+      createBlogMenu(blogMenu)
+    }
   </Menu>
 </div>
 }
--- src/router/path.ts
+++ src/router/path.ts
@@ -1,1 +1,48 @@
-export default {}
+import { Menu } from '@arco-design/web-react';
+import { IconApps, IconBulb } from '@arco-design/web-react/icon';
+import { Link } from 'react-router-dom';
+
+const MenuItem = Menu.Item;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup
+
+function createBlogMenuItem(compName: any, idx: string, title: string, children: any, icoName?: any,) {
+  return {
+    compName,
+    idx,
+    icoName,
+    title,
+    children
+  }
+}
+
+export default {
+  children: [
+    createBlogMenuItem(SubMenu, '0', 'dataStruct', [
+      createBlogMenuItem(MenuItem, '0_0', 'base', null),
+      createBlogMenuItem(MenuItem, '0_1', 'application', null)
+    ], IconApps),
+    createBlogMenuItem(SubMenu, '1', 'Mx', [
+      createBlogMenuItem(SubMenu, '1_0', 'Mx1', [
+        createBlogMenuItem(MenuItemGroup, '1_0_0', 'Mx11', [
+          createBlogMenuItem(Link, '/', '', [
+            createBlogMenuItem(MenuItem, '1_0_0_0', 'Mx111', null)
+          ]),
+          createBlogMenuItem(Link, '/jl', '', [
+            createBlogMenuItem(MenuItem, '1_0_0_1', 'Mx112', null)    
+          ])
+        ])
+      ]),
+      createBlogMenuItem(SubMenu, '1_1', 'Mx2', [
+        createBlogMenuItem(MenuItemGroup, '1_1_0', 'Mx22', [
+          createBlogMenuItem(Link, '/blog', '', [
+            createBlogMenuItem(MenuItem, '1_1_0_0', 'Mx222', null)
+          ]),
+          createBlogMenuItem(Link, '/tool', '', [
+            createBlogMenuItem(MenuItem, '1_1_0_1', 'Mx223', null)    
+          ])
+        ])
+      ])
+    ], IconBulb)
+  ]
+}
