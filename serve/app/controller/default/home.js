152dc81a2abca7183cda4845984175de25f8d208
huanghao <1308936944@qq.com>
2022-01-08 16:27:27 +0800

    动态查询测试

--- serve/app/controller/default/home.js
+++ serve/app/controller/default/home.js
@@ -9,10 +9,10 @@ class HomeController extends Controller {
   }
 
   async getArticleList() {
-    let sql = 'SELECT article.id as id ,' +
+    const sql = 'SELECT article.id as id ,' +
               'article.title as title ,' +
               'article.introduce as introduce ,' +
-              'article.addTime as addTime ,' +
+              "FROM_UNIXTIME(article.addTime, '%Y-%m-%d %H:%i:%s') as addTime ," +
               'article.view_count as view_count ,' +
               'type.typeName as typeName ' +
               'FROM article LEFT JOIN type ON article.type_id = type.id'
@@ -21,6 +21,21 @@ class HomeController extends Controller {
     this.ctx.body = { data: results }
   }
 
+  async getArticleById() {
+    const id = this.ctx.params.id
+    const sql = 'SELECT article.id as id ,' +
+                'article.title as title ,' +
+                'article.introduce as introduce ,' +
+                'article.article_content as article_content, ' +
+                "FROM_UNIXTIME(article.addTime, '%Y-%m-%d %H:%i:%s') as addTime ," +
+                'article.view_count as view_count ,' +
+                'type.typeName as typeName, ' +
+                'type.id as typeId ' +
+                'FROM article LEFT JOIN type ON article.type_id = type.id ' +
+                'WHERE article.id = ' + id
+    const results = await this.app.mysql.query(sql); 
+    this.ctx.body = { data: results };
+  }
 
 }
 
