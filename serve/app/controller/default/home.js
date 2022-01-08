dad9b6782534e7ff3ea535eb40d4e0be0b9b4fd6
huanghao <1308936944@qq.com>
2022-01-08 16:02:38 +0800

    数据库连接测试

--- serve/app/controller/default/home.js
+++ serve/app/controller/default/home.js
@@ -7,6 +7,21 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, eggsssss';
   }
+
+  async getArticleList() {
+    let sql = 'SELECT article.id as id ,' +
+              'article.title as title ,' +
+              'article.introduce as introduce ,' +
+              'article.addTime as addTime ,' +
+              'article.view_count as view_count ,' +
+              'type.typeName as typeName ' +
+              'FROM article LEFT JOIN type ON article.type_id = type.id'
+  
+    const results = await this.app.mysql.query(sql);
+    this.ctx.body = { data: results }
+  }
+
+
 }
 
 module.exports = HomeController;
