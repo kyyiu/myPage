8aca2e3823b0d743d6d54c3f6926e5ef6597955a
huanghao <1308936944@qq.com>
2022-02-11 16:54:33 +0800

    博客页菜单栏大体内容

--- src/router/path.ts
+++ src/router/path.ts
@@ -43,6 +43,70 @@ export default {
           ])
         ])
       ])
+    ], IconBulb),
+    createBlogMenuItem(SubMenu, '2', '读书记录', [
+      createBlogMenuItem(SubMenu, '2_0', '红宝书(js高级程序设计4)', [
+        createBlogMenuItem(MenuItemGroup, '2_0_0', '第一章', [
+          createBlogMenuItem(Link, '/', '', [
+            createBlogMenuItem(MenuItem, '2_0_0_0', '变量', null)
+          ]),
+          createBlogMenuItem(Link, '/jl', '', [
+            createBlogMenuItem(MenuItem, '2_0_0_1', '函数', null)    
+          ])
+        ])
+      ]),
+      createBlogMenuItem(SubMenu, '2_1', '易经', [
+        createBlogMenuItem(MenuItemGroup, '2_1_0', '第一章', [
+          createBlogMenuItem(Link, '/blog/1', '', [
+            createBlogMenuItem(MenuItem, '2_1_0_0', '什么是易经', null)
+          ]),
+          createBlogMenuItem(Link, '/blog/2', '', [
+            createBlogMenuItem(MenuItem, '2_1_0_1', '中国人的文化根源', null)    
+          ])
+        ])
+      ])
+    ], IconBulb),
+    createBlogMenuItem(SubMenu, '3', '博客', [
+      createBlogMenuItem(SubMenu, '3_0', 'js', [
+        createBlogMenuItem(MenuItemGroup, '3_0_0', '基本', [
+          createBlogMenuItem(Link, '/', '', [
+            createBlogMenuItem(MenuItem, '3_0_0_0', '变量', null)
+          ]),
+          createBlogMenuItem(Link, '/jl', '', [
+            createBlogMenuItem(MenuItem, '3_0_0_1', '函数', null)    
+          ])
+        ])
+      ]),
+      createBlogMenuItem(SubMenu, '3_1', 'css', [
+        createBlogMenuItem(MenuItemGroup, '3_1_0', '选择器', [
+          createBlogMenuItem(Link, '/blog/1', '', [
+            createBlogMenuItem(MenuItem, '3_1_0_0', '普通', null)
+          ]),
+          createBlogMenuItem(Link, '/blog/2', '', [
+            createBlogMenuItem(MenuItem, '3_1_0_1', '特殊', null)    
+          ])
+        ])
+      ]),
+      createBlogMenuItem(SubMenu, '3_2', '源码', [
+        createBlogMenuItem(MenuItemGroup, '3_2_0', '框架', [
+          createBlogMenuItem(Link, '/blog/1', '', [
+            createBlogMenuItem(MenuItem, '3_2_0_0', 'react', null)
+          ]),
+          createBlogMenuItem(Link, '/blog/2', '', [
+            createBlogMenuItem(MenuItem, '3_2_0_1', 'vue', null)    
+          ])
+        ])
+      ]),
+      createBlogMenuItem(SubMenu, '3_3', '其他', [
+        createBlogMenuItem(MenuItemGroup, '3_3_0', '其他', [
+          createBlogMenuItem(Link, '/blog/1', '', [
+            createBlogMenuItem(MenuItem, '3_3_0_0', '其他', null)
+          ]),
+          createBlogMenuItem(Link, '/blog/2', '', [
+            createBlogMenuItem(MenuItem, '3_3_0_1', '其他', null)    
+          ])
+        ])
+      ])
     ], IconBulb)
   ]
 }
