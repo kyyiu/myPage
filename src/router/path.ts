0eb434b6c820edd976e7f53c2c0ef34b2448f1fb
huanghao <1308936944@qq.com>
2022-03-09 20:58:56 +0800

    命名参数可扩展路由函数

--- src/router/path.ts
+++ src/router/path.ts
@@ -6,7 +6,7 @@ const MenuItem = Menu.Item;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup
 
-function createBlogMenuItem(compName: any, idx: string, title: string, children: any, icoName?: any, {config={}}:{config: Object}={config: {}}) {
+function createBlogMenuItem(compName: any, idx: string, title: string, children: any, {config={}, icoName=null}:{config?: Object, icoName?: any}={config: {}}) {
   return {
     compName,
     idx,
@@ -22,38 +22,38 @@ const blogPath = {
     createBlogMenuItem(SubMenu, '0', 'dataStruct', [
       createBlogMenuItem(MenuItem, '0_0', 'base', null),
       createBlogMenuItem(MenuItem, '0_1', 'application', null)
-    ], IconApps),
+    ], {icoName: IconApps}),
     createBlogMenuItem(SubMenu, '1', 'translate', [
       createBlogMenuItem(SubMenu, '1_0', 'webrix', [
         createBlogMenuItem(MenuItemGroup, '1_0_0', 'comp', [
           createBlogMenuItem(Link, '/translate/1', '', [
             createBlogMenuItem(MenuItem, '1_0_0_0', 'c1', null)
-          ]),
+          ], {config: { to: '2_1_0'}}),
           createBlogMenuItem(Link, '/translate/2', '', [
             createBlogMenuItem(MenuItem, '1_0_0_1', 'c2', null)    
-          ])
+          ], {config: { to: '2_1_0'}})
         ])
       ]),
       createBlogMenuItem(SubMenu, '1_1', 'axios', [
         createBlogMenuItem(MenuItemGroup, '1_1_0', 'main', [
           createBlogMenuItem(Link, '/axios/1', '', [
             createBlogMenuItem(MenuItem, '1_1_0_0', 'm1', null)
-          ]),
+          ], {config: { to: '2_1_0'}}),
           createBlogMenuItem(Link, '/axios/2', '', [
             createBlogMenuItem(MenuItem, '1_1_0_1', 'm2', null)    
-          ])
+          ], {config: { to: '2_1_0'}})
         ])
       ])
-    ], IconBulb),
+    ], {icoName: IconApps}),
     createBlogMenuItem(SubMenu, '2', '读书记录', [
       createBlogMenuItem(SubMenu, '2_0', '红宝书(js高级程序设计4)', [
         createBlogMenuItem(MenuItemGroup, '2_0_0', '第一章', [
           createBlogMenuItem(Link, '/jsBook/var', '', [
             createBlogMenuItem(MenuItem, '2_0_0_0', '变量', null)
-          ]),
+          ], {config: { to: '2_1_0'}}),
           createBlogMenuItem(Link, '/jsBook/func', '', [
             createBlogMenuItem(MenuItem, '2_0_0_1', '函数', null)    
-          ])
+          ], {config: { to: '2_1_0'}})
         ])
       ]),
       createBlogMenuItem(SubMenu, '2_1', '易经', [
@@ -63,52 +63,52 @@ const blogPath = {
           ], {config: { to: '2_1_0'}}),
           createBlogMenuItem(Link, '/yi/chinese', '', [
             createBlogMenuItem(MenuItem, '2_1_0_1', '中国人的文化根源', null)    
-          ])
+          ], {config: { to: '2_1_0'}})
         ])
       ])
-    ], IconBulb),
+    ], {icoName: IconApps}),
     createBlogMenuItem(SubMenu, '3', '博客', [
       createBlogMenuItem(SubMenu, '3_0', 'js', [
         createBlogMenuItem(MenuItemGroup, '3_0_0', '基本', [
           createBlogMenuItem(Link, '/blog/base', '', [
             createBlogMenuItem(MenuItem, '3_0_0_0', '变量', null)
-          ]),
+          ], {config: { to: '2_1_0'}}),
           createBlogMenuItem(Link, '/blog/base2', '', [
             createBlogMenuItem(MenuItem, '3_0_0_1', '函数', null)    
-          ])
+          ], {config: { to: '2_1_0'}})
         ])
       ]),
       createBlogMenuItem(SubMenu, '3_1', 'css', [
         createBlogMenuItem(MenuItemGroup, '3_1_0', '选择器', [
           createBlogMenuItem(Link, '/css/normal', '', [
             createBlogMenuItem(MenuItem, '3_1_0_0', '普通', null)
-          ]),
+          ], {config: { to: '2_1_0'}}),
           createBlogMenuItem(Link, '/css/special', '', [
             createBlogMenuItem(MenuItem, '3_1_0_1', '特殊', null)    
-          ])
+          ], {config: { to: '2_1_0'}})
         ])
       ]),
       createBlogMenuItem(SubMenu, '3_2', '源码', [
         createBlogMenuItem(MenuItemGroup, '3_2_0', '框架', [
           createBlogMenuItem(Link, '/blog/1', '', [
             createBlogMenuItem(MenuItem, '3_2_0_0', 'react', null)
-          ]),
+          ], {config: { to: '2_1_0'}}),
           createBlogMenuItem(Link, '/blog/2', '', [
             createBlogMenuItem(MenuItem, '3_2_0_1', 'vue', null)    
-          ])
+          ], {config: { to: '2_1_0'}})
         ])
       ]),
       createBlogMenuItem(SubMenu, '3_3', '其他', [
         createBlogMenuItem(MenuItemGroup, '3_3_0', '其他', [
           createBlogMenuItem(Link, '/else/1', '', [
             createBlogMenuItem(MenuItem, '3_3_0_0', '其他', null)
-          ]),
+          ], {config: { to: '2_1_0'}}),
           createBlogMenuItem(Link, '/else/2', '', [
             createBlogMenuItem(MenuItem, '3_3_0_1', '其他', null)    
-          ])
+          ], {config: { to: '2_1_0'}})
         ])
       ])
-    ], IconBulb)
+    ], {icoName: IconApps})
   ]
 }
 
@@ -123,7 +123,7 @@ export const webrixPath = {
       createBlogMenuItem(Link, '/webrix/en', '', [
         createBlogMenuItem(MenuItem, '0_1', '英文文档', null)
       ])
-    ], IconApps),
+    ], {icoName: IconApps}),
     createBlogMenuItem(SubMenu, '1', '使用', [
       createBlogMenuItem(SubMenu, '1_0', 'Movable', [
         createBlogMenuItem(MenuItemGroup, '1_0_0', '例子', [
@@ -135,7 +135,7 @@ export const webrixPath = {
           ])
         ])
       ])
-    ], IconBulb)
+    ], {icoName: IconApps})
   ]
 }
 
@@ -148,7 +148,7 @@ export const mediaPath = {
       createBlogMenuItem(Link, '/pics/recommand', '', [
         createBlogMenuItem(MenuItem, '0_1', '推荐', null)
       ])
-    ], IconApps),
+    ], {icoName: IconApps}),
     createBlogMenuItem(SubMenu, '1', '音乐', [
       createBlogMenuItem(Link, '/music/all', '', [
         createBlogMenuItem(MenuItem, '1_0', '全部', null)
@@ -156,7 +156,7 @@ export const mediaPath = {
       createBlogMenuItem(Link, '/music/favo', '', [
         createBlogMenuItem(MenuItem, '1_1', '喜欢', null)
       ])
-    ], IconBulb),
+    ], {icoName: IconApps}),
     createBlogMenuItem(SubMenu, '2', '视频', [
       createBlogMenuItem(Link, '/video/all', '', [
         createBlogMenuItem(MenuItem, '2_0', '全部', null)
@@ -164,7 +164,7 @@ export const mediaPath = {
       createBlogMenuItem(Link, '/video/favo', '', [
         createBlogMenuItem(MenuItem, '2_1', '喜欢', null)
       ])
-    ], IconBulb),
+    ], {icoName: IconApps}),
     createBlogMenuItem(SubMenu, '3', '视频', [
       createBlogMenuItem(Link, '/game/all', '', [
         createBlogMenuItem(MenuItem, '3_0', '全部', null)
@@ -172,6 +172,6 @@ export const mediaPath = {
       createBlogMenuItem(Link, '/game/favo', '', [
         createBlogMenuItem(MenuItem, '3_1', '喜欢', null)
       ])
-    ], IconBulb),
+    ], {icoName: IconApps}),
   ]
 }
